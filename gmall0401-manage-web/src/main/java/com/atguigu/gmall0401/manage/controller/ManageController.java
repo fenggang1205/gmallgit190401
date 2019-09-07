beb2f62f9c23210fb77fc47d6dec83384b50c83d
fenggang <1479843655@qq.com>
2019-09-07 14:10:42 +0800

    修改readme

---
+++ gmall0401-manage-web/src/main/java/com/atguigu/gmall0401/manage/controller/ManageController.java
@@ -0,0 +1,50 @@
+package com.atguigu.gmall0401.manage.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall0401.bean.*;
+import com.atguigu.gmall0401.service.ManageService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class ManageController {
+
+    @Reference
+    ManageService manageService;
+    
+    @PostMapping("getCatalog1")
+    public List<BaseCatalog1> getCatalog1(){
+        List<BaseCatalog1> baseCatalog1List = manageService.getCatalog1();
+        return baseCatalog1List;
+    }
+
+    @PostMapping("getCatalog2")
+    public List<BaseCatalog2> getCatalog2(String catalog1Id){
+        List<BaseCatalog2> baseCatalog2List = manageService.getCatalog2(catalog1Id);
+        return baseCatalog2List;
+    }
+    @PostMapping("getAttrValueList")
+    public List<BaseAttrValue> getAttrValueList(String attrId){
+        BaseAttrInfo baseAttrInfo = manageService.getBaseAttrInfo(attrId);
+        List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
+        return attrValueList;
+    }
+
+    @PostMapping("getCatalog3")
+    public List<BaseCatalog3> getCatalog3(String catalog2Id){
+        List<BaseCatalog3> baseCatalog3List = manageService.getCatalog3(catalog2Id);
+        return baseCatalog3List;
+    }
+    @GetMapping("attrInfoList")
+    public List<BaseAttrInfo> getattrInfoList(String catalog3Id){
+        List<BaseAttrInfo> baseAttrInfoList = manageService.getAttrList(catalog3Id);
+        return baseAttrInfoList;
+    }
+    @PostMapping("saveAttrInfo")
+    public String save(@RequestBody BaseAttrInfo baseAttrInfo){
+        manageService.saveAttrInfo(baseAttrInfo);
+        return "success";
+    }
+}
