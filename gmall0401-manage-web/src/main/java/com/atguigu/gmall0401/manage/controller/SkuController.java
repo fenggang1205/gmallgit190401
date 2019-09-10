36f70bbb2910f4cdc4d79d8c2df4835136fbf497
fenggang <1479843655@qq.com>
2019-09-10 22:51:16 +0800

    ok11123

---
+++ gmall0401-manage-web/src/main/java/com/atguigu/gmall0401/manage/controller/SkuController.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall0401.manage.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall0401.bean.SkuInfo;
+import com.atguigu.gmall0401.service.ManageService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class SkuController {
+
+    @Reference
+    ManageService manageService;
+
+    @PostMapping("saveSkuInfo")
+    public String saveSkuInfo(@RequestBody SkuInfo skuInfo) {
+
+        manageService.saveSpuInfo(skuInfo);
+        return "success";
+    }
+}
